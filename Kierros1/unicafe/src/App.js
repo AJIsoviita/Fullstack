567ba91ea089b46f082c78fac493800bbfe81637
Antti-Jussi Isoviita <antti-jussi.isoviita@tuni.fi>
2021-09-27 16:00:11 +0300

    1.6 started

---
+++ Kierros1/unicafe/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(1)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(-1)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
