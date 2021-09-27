f0caf75c6e038faaaef425d9aa54935402966ec2
Antti-Jussi Isoviita <antti-jussi.isoviita@tuni.fi>
2021-09-27 15:55:27 +0300

    Tehtävät 1.1-1.5

--- Kierros1/kurssitiedot/src/App.js
+++ Kierros1/kurssitiedot/src/App.js
@@ -9,7 +9,6 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props)
   return (
     <div>
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
